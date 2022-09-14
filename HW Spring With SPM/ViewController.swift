810a27e98d779919934d49ffef227594ffdde872
Pttr <draivv@yandex.ru>
2022-09-14 19:33:28 +0300

    Initial Commit

---
+++ HW Spring With SPM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW Spring With SPM
+//
+//  Created by P Z on 14/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
