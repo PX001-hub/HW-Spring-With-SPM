7f96bb438e56347245aba9c3b5231befe0feeb1b
Pttr <draivv@yandex.ru>
2022-09-15 10:45:10 +0300

    1.6

--- HW Spring With SPM/ViewController.swift
+++ HW Spring With SPM/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var coreAnimationView: UIView!
     
+    private var animationStarted = false
     
     @IBAction func runCoreAnimation(_ sender: UIButton) {
         
@@ -18,7 +19,11 @@ class ViewController: UIViewController {
             withDuration: 0.5,
             delay: 0,
             options: [.autoreverse, .repeat]) { [unowned self] in
+                if !animationStarted {
                 coreAnimationView.frame.origin.x -= 40
+                    animationStarted.toggle()
+                }
+                
         }
         
         sender.pulsate()
